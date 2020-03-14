9fd101308c50a35e3b5871468fe9bcf2d1741f36
Tyler Gasperlin <gasperlint@gmail.com>
2020-03-14 11:53:57 -0700

    InitialCommit

---
+++ validator.js
@@ -0,0 +1,41 @@
+//to create a validator
+db.createCollection('posts', {
+    validator: {
+        $jsonSchema: {
+            bsonType: 'object',
+            required: ['title','text','creator','comments'],
+            properties: {
+                title: {
+                    bsonType: "string",
+                    description: "must be a string and is required"
+                },
+                text:{
+                    bsonType:"string",
+                    description: "must be a string and is required"
+                },
+                creator:{
+                    bsonType: "objectId",
+                    description: "must be an objectidand is required"
+                },
+                comments:{
+                    bsonType:"array",
+                    description:"must be an array and is required",
+                    items: {
+                        bsonType: "object",
+                        required: ["text", "author"],
+                        properties: {
+                            text: {
+                                bsonType: "string",
+                                description: "must be a string and is required"
+                            },
+                            author: {
+                                bsonType: "objectId",
+                                description: 'must be a string and is required'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
